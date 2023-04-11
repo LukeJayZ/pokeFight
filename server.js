445914dacbfe948b758211bf8c484607ffb2f6c6
JorgoPapapostolu <jorgopapapostolu@gmail.com>
2023-04-11 17:58:38 +0200

    part 1

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
-const express = require('express')
-const app = express()
-const router = require('./Router/pokemon'); 
+const express = require("express");
+const app = express();
+const router = require("./Router/pokemon");
+const cors = require('cors');
+
+app.use(cors());
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-
-app.use('/pokemon', router);
-
+app.use("/pokemon", router);
 
 module.exports = app;
