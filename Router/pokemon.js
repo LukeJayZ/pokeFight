c0304722454982bc6312111359c9aeb54885e3ff
Lukas <git@github.com>
2023-04-12 15:10:08 +0200

    Router merge conflict gelöst

--- Router/pokemon.js
+++ Router/pokemon.js
@@ -3,10 +3,6 @@ const router = express.Router();
 const pokemonController = require("../Controller/controller");
 
 router.get("/", pokemonController.getPokemon);
-<<<<<<< HEAD
-router
-=======
 router.get("/:id", pokemonController.getPokemonById);
->>>>>>> 0b7086c5b4a56236eb5b2eaf847abe28683a539f
 
 module.exports = router;
