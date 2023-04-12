38c728d0beafaf26b2eb988b8cf78c432e9653d4
Lukas <git@github.com>
2023-04-12 14:57:12 +0200

    package.json start hinzugefügt

--- Controller/controller.js
+++ Controller/controller.js
@@ -4,3 +4,8 @@ exports.getPokemon = (req, res) => {
     res.json(pokedex)
 }
 
+exports.getPokemonById = (req, res) =>{
+    const id = parseInt(req.params.id);
+    const pokemon = pokedex.pokemon.find(p => p.id === id);
+    res.json(pokemon);
+};
--- Router/pokemon.js
+++ Router/pokemon.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const pokemonController = require("../Controller/controller");
 
 router.get("/", pokemonController.getPokemon);
+router
 
 module.exports = router;
