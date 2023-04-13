435313ce42ff22b371a2cd268c1bbd7ac69f532b
Lukas <git@github.com>
2023-04-13 16:07:34 +0200

    frontend routes

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,9 +1,32 @@
 
+/* import {Route, Routes} from 'react-router-dom'; */
 import './App.css';
+import {useState, useEffect} from 'react'
 
 function App() {
+  const [getPokemon,setGetPokemon] = useState(null)
+  const fetchData = async () => {
+    try {
+      const pokemonData = await fetch("https://pokefight-lrha.onrender.com")
+      if (!pokemonData) throw new Error (`Request failed with the status of ${pokemonData.status}`)
+      const parseData = await pokemonData.json()
+      setGetPokemon(parseData)
+    } catch (error) {
+      console.log(error.message)
+      
+    }
+  }
+  useEffect(()=> {
+    fetchData()
+  },[])
+console.log(getPokemon);
   return (
     <div className="App">
+    {/*   <Routes>
+          <Route path = "/" element={AllPokemon}/>
+          <Route path = "/pokemon/:id" element= {PokemonId} />
+          <Route path = "/pokemon/:id/:info" element ={PokemonInfo}/>
+      </Routes> */}
 
     </div>
   );
